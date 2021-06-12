64aaf8485a9d81ac78837bc0a88e2cbad339b3ef
Mathias Estrup <mestru17@student.aau.dk>
2021-06-12 19:36:20 +0200

    Fix fish config being a symlink

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,25 @@
-/home/mathias/src/github.com/mestru17/dotfiles/.config/fish/config.fish
+
+### SET ENV VARIABLES ###
+set fish_greeting         # Supresses fish's intro message
+set TERM "xterm-256color" # Sets the terminal type
+set EDITOR "nvim"         # $EDITOR use NeoVim in terminal
+set VISUAL "nvim"         # $VISUAL use NeoVim in GUI mode
+set -x MANPAGER "sh -c 'col -bx | batcat -l man -p'"
+
+### ADD TO PATH ###
+fish_add_path $HOME/bin        # User binaries
+fish_add_path $HOME/nvim/bin   # NeoVim
+fish_add_path $HOME/.cargo/env # Rust
+
+### ALIASES ###
+# bat
+alias bat="batcat"
+
+# Change ls to exa
+alias ls='exa -al --header --color=always --group-directories-first --git --icons'
+alias ll='exa -l --header --color=always --group-directories-first --git --icons'
+alias lt='exa -aT --color=always --group-directories-first --icons'
+
+# Dotfiles tracking
+alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
