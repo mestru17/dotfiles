695d179ff9c5e6d5141387d9f9ea353a132a1a77
Mathias Estrup <mestru17@student.aau.dk>
2021-07-01 14:58:31 +0200

    Add LSP code action keybindings to NeoVim

--- .config/nvim/lua/pl-lspconfig/init.lua
+++ .config/nvim/lua/pl-lspconfig/init.lua
@@ -4,6 +4,9 @@ vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
 vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
 vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
 vim.cmd("nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>")
+vim.cmd("nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>")
+vim.cmd("nnoremap <silent> <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.cmd("vnoremap <silent> <leader>a <cmd>lua vim.lsp.buf.code_action()<CR>")
 
 -- Auto format
 vim.api.nvim_command([[autocmd BufWritePre *.rs lua vim.lsp.buf.formatting_sync(nil, 100)]])
